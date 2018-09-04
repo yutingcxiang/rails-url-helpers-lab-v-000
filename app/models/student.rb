1144b4f342113f41bae241206703543a01abf961
Christine Xiang <yutingcxiang@gmail.com>
2018-09-04 02:08:02 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,25 +2,4 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-  
-  def index
-    @studet = Student.all
-  end
-  
-  def show
-    @student = Student.find_by(id: params[:id])
-  end
-
-  def activate
-    @student = Student.find_by(id: params[:id])
-    
-    if @student.active == true
-      @student.active = false
-    else
-      @student.active = true
-    end
-    
-    @student.save
-    redirect_to student_path(@student)
-  end
 end
