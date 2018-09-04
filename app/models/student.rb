22a18f665d247c6c52192e8099e0fd902454bcb0
Christine Xiang <yutingcxiang@gmail.com>
2018-09-04 02:00:45 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def index
+    @studet = Student.all
+  end
+  
+  def show
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def 
+    @student = Student.find(params[:id])
+  end
 end
